54a0850677db9bbad11f5905f074e5db19614f40
melabreu <melissa.abreu84@gmail.com>
2025-10-07 18:29:14 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className='container'>
+      <h1>Melissa Abreu</h1>
+      <h3>Software Engineer</h3>
+      <hr />
+    </div>
+  );
+};
+
+export default App;
